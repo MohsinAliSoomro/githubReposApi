a83badce0c5f84613fa7115fefb86af80b6cb40c
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-07-08 15:26:42 +0500

    My initial repose website of

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
+  const [repos, setRepos] = useState([{}])
+  useEffect(() => {
+    async function FetchApi() {
+      await fetch('https://api.github.com/users/mohsinalisoomro/repos')
+        .then(res => res.json())
+        .then(result => {
+          console.log(result)
+          setRepos(result);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    FetchApi()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="maincontainer">
+      <div className="intro">
+        <h1>Mohsin Ali Soomro</h1>
+        <p>I am mern stack developer <br/>
+        Link <a href="https://github.com/MohsinAliSoomro">GitHub</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      {/* <ul>
+      {repos.map((item,ind)=>{
+        return(<li key={ind}>{item.name}</li>)
+      })}
+      </ul> */}
+      <div className="container">
+        {repos.map((item, ind) => {
+          return (
+            <div key={ind} className="display">
+              <h4 >Repos Name : <br />{item.name}</h4>
+              <h5>Full Name : {item.full_name}</h5>
+              <h5>language : {item.language}</h5>
+              <p>Created At : <br />{item.created_at}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
